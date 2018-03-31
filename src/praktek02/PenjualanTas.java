03f0cda540ced10eec481dfb815d428eed8fbd0d
Laila_Agustina <lailaagustina025@gmail.com>
2018-03-31 00:35:13 -0700

    Membuat Atribut dan Behavior

--- src/praktek02/PenjualanTas.java
+++ src/praktek02/PenjualanTas.java
@@ -1,4 +1,12 @@
 package praktek02;
 public class PenjualanTas {
-    
+  int hrgtas;
+  int jmlbeli;
+  
+  void cetakInfo(){
+     System.out.println("=====================");
+     System.out.println("hrgtas   : "+hrgtas);
+     System.out.println("jmlbeli  : "+jmlbeli);
+     System.out.println("=====================");
+  }
 }
