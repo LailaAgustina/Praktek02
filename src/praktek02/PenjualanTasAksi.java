f25f55198807025105096933d9c8066a2f34cb3f
Laila_Agustina <lailaagustina025@gmail.com>
2018-03-31 00:39:17 -0700

    Mengisi Nilai Atribut

--- src/praktek02/PenjualanTasAksi.java
+++ src/praktek02/PenjualanTasAksi.java
@@ -2,6 +2,7 @@ package praktek02;
 public class PenjualanTasAksi {
     public static void main(String[] args) {
         PenjualanTas r1 = new PenjualanTas();
-        
+        r1.hrgtas = 150000;
+        r1.jmlbeli = 3;
     }
 }
